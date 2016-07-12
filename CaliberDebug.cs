23ed466c5b00e97757af193d5104c818e3d8efad
iSO <LifeRPGiSO@gmail.com>
2016-07-12 22:08:59 +0100

    Base
    Base

---
+++ CaliberDebug.cs
@@ -0,0 +1,28 @@
+﻿using GTA;
+using System;
+using System.Windows.Forms;
+
+public class $safeprojectname$ : Script
+{
+    public $safeprojectname$()
+    {
+        Tick += OnTick;
+        KeyDown += OnKeyDown;
+        KeyUp += OnKeyUp;
+
+        Interval = 10; /* loop to check, ms */
+    }
+
+    void OnTick(object sender, EventArgs e)
+    {
+
+    }
+    void OnKeyDown(object sender, KeyEventArgs e)
+    {
+
+    }
+    void OnKeyUp(object sender, KeyEventArgs e)
+    {
+
+    }
+}
